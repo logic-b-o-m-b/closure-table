8960baff9a147ea193767e420be8bbd994acc379
logic-b-o-m-b <55141760+logic-b-o-m-b@users.noreply.github.com>
2021-12-11 16:15:04 +0000

    categories closure table

---
+++ src/Model/Table/CategoriesClosureTable.php
@@ -0,0 +1,69 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+class CategoriesClosureTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('categories_closure');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->integer('parent_id')
+            ->allowEmpty('parent_id');
+
+        $validator
+            ->integer('child_id')
+            ->allowEmpty('child_id');
+
+        $validator
+            ->integer('depth')
+            ->allowEmpty('depth');
+
+        $validator
+            ->integer('node_order')
+            ->allowEmpty('node_order');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        return $rules;
+    }
+}
